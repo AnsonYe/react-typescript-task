dc6a6f9223a6ba357b56e67641ca68d13a797f38
AnsonYe <anson.ye@gamil.com>
2023-07-08 21:12:06 +0900

    Add DragDrop Completed Task

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,23 +1,65 @@
 import React from "react";
 import { Todo } from "../models/models";
 import SingleTodo from "./SingleTodo";
+import { Droppable } from "react-beautiful-dnd";
 
 interface props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  CompletedTodos: Array<Todo>;
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
 }
 
-const TodoList = ({ todos, setTodos }: props) => {
+const TodoList = ({
+  todos,
+  setTodos,
+  CompletedTodos,
+  setCompletedTodos,
+}: props) => {
   return (
-    <div className="todos">
-      {todos?.map((todo) => (
-        <SingleTodo
-          todos={todos}
-          todo={todo}
-          key={todo.id}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      <Droppable droppableId="TodosList">
+        {(provided) => (
+          <div
+            className="todos"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos__heading">Active Tasks</span>
+            {todos?.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todos={todos}
+                todo={todo}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="TodosRemove">
+        {(provided) => (
+          <div
+            className="todos remove"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos__heading">Completed Tasks</span>
+            {CompletedTodos?.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todos={CompletedTodos}
+                todo={todo}
+                key={todo.id}
+                setTodos={setCompletedTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
